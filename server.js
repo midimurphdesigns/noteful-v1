37642137cbdf82f21fd26a9e39dae06c74344f47
midimurphdesigns <midimurphdesigns@gmail.com>
2018-03-05 13:47:54 -0700

    got endpoints working

--- server.js
+++ server.js
@@ -2,7 +2,23 @@
 
 // TEMP: Simple In-Memory Database
 const data = require('./db/notes');
+const express = require('express');
+const app = express();
 
-console.log('hello world!');
+app.use(express.static('public'));
 
-// INSERT EXPRESS APP CODE HERE...
+// Listen for incoming connections
+app.listen(8080, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
+
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  res.json(data.find(item => item.id === Number(id)));
+});
