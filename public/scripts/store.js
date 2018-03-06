43fbbaf7f15b1b22d6c41953c9fef5ade9be0cb2
midimurphdesigns <midimurphdesigns@gmail.com>
2018-03-06 15:14:39 -0700

    modularized files and added functionality

--- public/scripts/store.js
+++ public/scripts/store.js
@@ -3,10 +3,16 @@
 
 const store = (function(){
 
+  const findAndUpdate = (id, newData) => {
+    const note = store.notes.find(note => note.id === id);
+    Object.assign(note, newData);
+  };
+
   return {
     notes: [],
     currentNote: false,
-    currentSearchTerm: ''
+    currentSearchTerm: '',
+    findAndUpdate,
   };
   
 }());
--- server.js
+++ server.js
@@ -4,27 +4,89 @@
 const data = require('./db/notes');
 const express = require('express');
 const app = express();
+const { PORT } = require('./config');
+const morgan = require('morgan');
+const simDB = require('./db/simDB');  // <<== add this
+const notes = simDB.initialize(data); // <<== and this
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(morgan('common'));
 
-// Listen for incoming connections
-app.listen(8080, function () {
-  console.info(`Server listening on ${this.address().port}`);
-}).on('error', err => {
-  console.error(err);
-});
-
-app.get('/api/notes', (req, res) => {
-  const searchTerm = req.query.searchTerm;
+app.get('/api/notes', (req, res, next) => {
+  const { searchTerm } = req.query;
   if (searchTerm) {
-    const filterBySearchTerm = data.filter(item => item.title.includes(searchTerm) || item.content.includes(searchTerm));
-    res.json(filterBySearchTerm);
+    notes.filter(searchTerm, (err, list) => {
+      if (err) {
+        return next(err);
+      }
+      res.json(list);
+    });
   } else {
     res.json(data);
   }
 });
 
-app.get('/api/notes/:id', (req, res) => {
+app.get('/api/notes/:id', (req, res, next) => {
   const id = req.params.id;
-  res.json(data.find(item => item.id === Number(id)));
+  notes.find(id, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.json(item);
+    } else {
+      console.log('not found');
+    }
+  });
+});
+
+app.put('/api/notes/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  /***** Never trust users - validate input *****/
+  const updateObj = {};
+  const updateFields = ['title', 'content'];
+
+  updateFields.forEach(field => {
+    if (field in req.body) {
+      updateObj[field] = req.body[field];
+    }
+  });
+
+  notes.update(id, updateObj, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.json(item);
+    } else {
+      next();
+    }
+  });
+});
+
+app.get('/boom', (req, res, next) => {
+  throw new Error('Boom!!');
+});
+
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found' });
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+
+// Listen for incoming connections
+app.listen(PORT, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
 });
